d68da26beae76d570e013cb3974fc5cfbce933ea
junayed_mim <karim.junayed@gmail.com>
2025-12-11 21:56:37 +0600

    added function to print to cli

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as book:
+        book_contents = book.read()
+        print(book_contents)
+        return
+    
+def main():
+    get_book_text("books/frankenstein.txt")
+
+
+
+main()
