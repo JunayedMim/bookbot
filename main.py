a0e4f48eb18384eeebc06ae2aec94a1cbc4303b2
junayed_mim <karim.junayed@gmail.com>
2025-12-12 00:20:23 +0600

    sort function work in progress

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_counter
+from stats import word_counter, character_counter, dict_sort
 
 
 def get_book_text(file_path):
@@ -16,7 +16,11 @@ def get_book_text(file_path):
 def main():
     book_contents = get_book_text("books/frankenstein.txt")
     word_count = word_counter(book_contents)
+    char_count = character_counter(book_contents)
+    sorted_count = dict_sort(char_count)
+    
     print(f"Found {word_count} total words")
+    print(sorted_count)
 
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,34 @@ def word_counter(book_contents):
     word_list = book_contents.split()
     num_words = len(word_list)
     return num_words
+
+
+def character_counter(book_contents):
+    char_dict = {}
+    word_list = book_contents.split()
+
+    for word in word_list:
+        lowered_word = word.lower()
+        
+        for char in lowered_word:
+            if char not in char_dict:
+                char_dict[char] = 1
+            elif char in char_dict:
+                char_dict[char] += 1
+
+    return char_dict
+
+
+def dict_sort(char_dict):
+    sorted_list = []
+    single_char_dict = {"char": "char_value", "num": "num_value"}
+    
+    for char in char_dict:
+        single_char_dict["char"] = char
+        single_char_dict["num"] = char_dict[char]
+        single_char_dict_sort = {single_char_dict["char"]: single_char_dict["num"]}
+
+        sorted_list.append(single_char_dict_sort)
+    
+    return sorted_list
+
